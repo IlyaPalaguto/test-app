18676d3977c6163cb86d69d17160e12abd43d2f4
Ilia <i.v.palaguto@gmail.com>
2022-09-20 21:16:38 +0500

    Corrected seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,94 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = Category.create([{title: 'Ruby'}, {title: 'JavaScript'}, {title: 'Python'}])
+categories = Category.create!([{title: 'Ruby'}, {title: 'JavaScript'}, {title: 'Python'}])
 users = User.create([{name: 'Ilia'}, {name: 'Alex'}, {name: 'John'}])
 
-tests = Test.create([
+tests = Test.create!([
   {title: 'Euismod nisi porta', author_id: users.first.id, category_id: categories.first.id, level: 5},
   {title: 'Est pellentesque elit ullamcorper', author_id: users.second.id, category_id: categories.second.id, level: 2},
   {title: 'Feugiat vivamus at', author_id: users.last.id, category_id: categories.last.id, level: 8}
 ])
 
-questions = Question.create([
+questions = Question.create!([
   {body: 'Nisl nunc mi ipsum faucibus vitae aliquet?', test_id: tests.first.id},
   {body: 'Lobortis feugiat vivamus at augue?', test_id: tests.second.id},
   {body: 'Feugiat vivamus at augue?', test_id: tests.last.id}
+  {body: 'Lobortis mattis aliquam faucibus purus in massa?', test_id: tests.first.id},
+  {body: 'Lornisl nunc mi ipsum?', test_id: tests.second.id},
+  {body: 'Lorlobortis feugiat vivamus at augue?', test_id: tests.last.id},
+  {body: 'Loraliquam ut porttitor?', test_id: tests.first.id},
+  {body: 'Lortortor vitae purus?', test_id: tests.second.id},
+  {body: 'Loreuismod nisi?', test_id: tests.last.id},
+  {body: 'Lornisi porta lorem?', test_id: tests.first.id},
+  {body: 'Lorfeugiat vivamus at?', test_id: tests.second.id},
+  {body: 'Loriverra tellus in hac habitasse platea?', test_id: tests.last.id},
+  {body: 'Lorrhoncus  elit ullamcorper dignissim cras?', test_id: tests.first.id},
+  {body: 'Lorlobortis feugiat?', test_id: tests.second.id},
+  {body: 'Lordictum varius duis at?', test_id: tests.last.id}
 ])
 
-answers = Answer.create([
-  {body: 'yes', question_id: questions.first.id, correct: false},
-  {body: 'no', question_id: questions.first.id, correct: true},
-  {body: 'yes', question_id: questions.second.id, correct: false},
-  {body: 'no', question_id: questions.second.id, correct: true},
-  {body: 'yes', question_id: questions.last.id, correct: false},
-  {body: 'no', question_id: questions.last.id, correct: true}
-])
-
-started_tests = StartedTest.create([
-  {user_id: users.first.id, test_id: tests.first.id},
-  {user_id: users.first.id, test_id: tests.second.id},
-  {user_id: users.first.id, test_id: tests.last.id},
-  {user_id: users.second.id, test_id: tests.first.id},
-  {user_id: users.second.id, test_id: tests.second.id},
-  {user_id: users.second.id, test_id: tests.last.id},
-  {user_id: users.last.id, test_id: tests.first.id},
-  {user_id: users.last.id, test_id: tests.second.id},
-  {user_id: users.last.id, test_id: tests.last.id}
+Answer.create!([
+  {body: 'yes', question_id: questions.[0].id, correct: false},
+  {body: 'no', question_id: questions.[0].id, correct: true},
+  {body: 'dont know', question_id: questions.[0].id, correct: false},
+  {body: 'sure', question_id: questions.[0].id, correct: false},
+  {body: 'yes', question_id: questions.[1].id, correct: false},
+  {body: 'no', question_id: questions.[1].id, correct: true},
+  {body: 'yes', question_id: questions.[2].id, correct: false},
+  {body: 'no', question_id: questions.[2].id, correct: true}
+  {body: 'yes', question_id: questions.[3].id, correct: false},
+  {body: 'no', question_id: questions.[3].id, correct: true},
+  {body: 'dont know', question_id: questions.[3].id, correct: false},
+  {body: 'sure', question_id: questions.[3].id, correct: false},
+  {body: 'yes', question_id: questions.[4].id, correct: false},
+  {body: 'no', question_id: questions.[4].id, correct: true},
+  {body: 'dont know', question_id: questions.[4].id, correct: false},
+  {body: 'sure', question_id: questions.[4].id, correct: false},
+  {body: 'yes', question_id: questions.[5].id, correct: false},
+  {body: 'no', question_id: questions.[5].id, correct: true},
+  {body: 'dont know', question_id: questions.[5].id, correct: false},
+  {body: 'sure', question_id: questions.[5].id, correct: false},
+  {body: 'yes', question_id: questions.[6].id, correct: false},
+  {body: 'no', question_id: questions.[6].id, correct: true},
+  {body: 'dont know', question_id: questions.[6].id, correct: false},
+  {body: 'sure', question_id: questions.[6].id, correct: false},
+  {body: 'yes', question_id: questions.[7].id, correct: false},
+  {body: 'no', question_id: questions.[7].id, correct: true},
+  {body: 'dont know', question_id: questions.[7].id, correct: false},
+  {body: 'sure', question_id: questions.[7].id, correct: false},
+  {body: 'yes', question_id: questions.[8].id, correct: false},
+  {body: 'no', question_id: questions.[8].id, correct: true},
+  {body: 'dont know', question_id: questions.[8].id, correct: false},
+  {body: 'sure', question_id: questions.[8].id, correct: false},
+  {body: 'yes', question_id: questions.[9].id, correct: false},
+  {body: 'no', question_id: questions.[9].id, correct: true},
+  {body: 'dont know', question_id: questions.[9].id, correct: false},
+  {body: 'sure', question_id: questions.[9].id, correct: false},
+  {body: 'yes', question_id: questions.[10].id, correct: false},
+  {body: 'no', question_id: questions.[10].id, correct: true},
+  {body: 'dont know', question_id: questions.[10].id, correct: false},
+  {body: 'sure', question_id: questions.[10].id, correct: false},
+  {body: 'yes', question_id: questions.[11].id, correct: false},
+  {body: 'no', question_id: questions.[11].id, correct: true},
+  {body: 'dont know', question_id: questions.[11].id, correct: false},
+  {body: 'sure', question_id: questions.[11].id, correct: false},
+  {body: 'yes', question_id: questions.[12].id, correct: false},
+  {body: 'no', question_id: questions.[12].id, correct: true},
+  {body: 'dont know', question_id: questions.[12].id, correct: false},
+  {body: 'sure', question_id: questions.[12].id, correct: false},
+  {body: 'yes', question_id: questions.[13].id, correct: false},
+  {body: 'no', question_id: questions.[13].id, correct: true},
+  {body: 'dont know', question_id: questions.[13].id, correct: false},
+  {body: 'sure', question_id: questions.[13].id, correct: false},
+  {body: 'yes', question_id: questions.[14].id, correct: false},
+  {body: 'no', question_id: questions.[14].id, correct: true},
+  {body: 'dont know', question_id: questions.[14].id, correct: false},
+  {body: 'sure', question_id: questions.[14].id, correct: false},
+  {body: 'yes', question_id: questions.[15].id, correct: false},
+  {body: 'no', question_id: questions.[15].id, correct: true},
+  {body: 'dont know', question_id: questions.[15].id, correct: false},
+  {body: 'sure', question_id: questions.[15].id, correct: false},
 ])
 
 # lorem_ipsum = %w[
