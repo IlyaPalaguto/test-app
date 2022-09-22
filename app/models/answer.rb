04a83eadbee603378ec3672a681a8def30f9c45b
Ilia <i.v.palaguto@gmail.com>
2022-09-23 03:29:49 +0500

    lesson 5 / feature: add scopes and validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,14 @@
 class Answer < ApplicationRecord
+  scope :correct, -> { where(correct: true) }
+  
   belongs_to :question
+
+  validates :body, presence: true
+  validate :validate_answers_quantity
+
+  private
+
+  def validate_answers_quantity
+    errors.add(:question) if question.answers.size > 3
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,27 @@
 class Test < ApplicationRecord
+  scope :level, -> (level) { where(level: level) }
+  scope :easy_levels, -> { level(0..1) }
+  scope :middle_levels, -> { level(2..4) }
+  scope :hard_levels, -> { level(5..Float::INFINITY) }
+  
   belongs_to :author, class_name: 'User'
   belongs_to :category
-
+  
   has_many :questions, dependent: :destroy
   has_many :started_tests, dependent: :destroy
   has_many :users, through: :started_tests, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true }
+  validate :validate_level_not_negative
   
   def self.show_tests(category)
-    Test.joins(:category).where(categories: {title: category}).order(title: :DESC).pluck(:title)
+    Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
+  end
+
+  private
+
+  def validate_level_not_negative
+    errors.add(:level) if level < 0
   end
 end
