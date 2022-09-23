17787055812b7e5598a838f9528ab19c0df0e871
Ilia <i.v.palaguto@gmail.com>
2022-09-23 17:20:26 +0500

    refactor: scopes and validates

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,14 @@
 class Answer < ApplicationRecord
+  belongs_to :question
+  
+  validates :body, presence: true
+  validate :validate_answers_quantity, on: :create
+  
   scope :correct, -> { where(correct: true) }
   
-  belongs_to :question
-
-  validates :body, presence: true
-  validate :validate_answers_quantity
-
   private
 
   def validate_answers_quantity
-    errors.add(:question) if question.answers.size > 3
+    errors.add(:question) if question.answers.size >= 4
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,22 +1,23 @@
 class Test < ApplicationRecord
-  scope :level, -> (level) { where(level: level) }
-  scope :easy_levels, -> { level(0..1) }
-  scope :middle_levels, -> { level(2..4) }
-  scope :hard_levels, -> { level(5..Float::INFINITY) }
-  
   belongs_to :author, class_name: 'User'
   belongs_to :category
   
   has_many :questions, dependent: :destroy
   has_many :started_tests, dependent: :destroy
   has_many :users, through: :started_tests, dependent: :destroy
-
+  
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true }
   validate :validate_level_not_negative
   
+  scope :level, -> (level) { where(level: level) }
+  scope :easy_levels, -> { level(0..1) }
+  scope :middle_levels, -> { level(2..4) }
+  scope :hard_levels, -> { level(5..Float::INFINITY) }
+  scope :with_category, -> (category) { joins(:category).where(categories: {title: category}) }
+  
   def self.show_tests(category)
-    Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
+    Test.with_category(category).order(title: :desc).pluck(:title)
   end
 
   private
