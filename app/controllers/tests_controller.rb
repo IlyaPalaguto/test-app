44eaab49990a9af919950c465b495b1b01ffc55c
Ilia <i.v.palaguto@gmail.com>
2022-09-27 17:50:40 +0500

    refactor: I hope its last :D

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,14 +1,6 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show]
-
   def show
+    @test = Test.find(params[:id])
     render inline: '<h1><%= "#{@test.title}, level: #{@test.level}" %></h1>'
   end
-
-  private
-
-  def find_test
-    @test = Test.find(params[:id])
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#author'
 
   resources :tests do
     resources :questions, shallow: true
   end
-  
-  get '/author', to: 'static_pages#author'
-  get '/about', to: 'static_pages#about'
 end
