9cfbe66abaa035f577d5a49c328368bb0bc9cedb
Ilia <i.v.palaguto@gmail.com>
2022-10-12 17:37:27 +0500

    Added dotenv gem

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "faraday"
 gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
+  gem 'dotenv-rails'
+  
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -3,15 +3,12 @@ require 'I18n'
 
 class GistQuestionService
 
-  # ACCESS_TOKEN = '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'
-  ACCESS_TOKEN = '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'
-
   attr_reader :client
 
   def initialize(question, client: nil)
     @question = question
     @test = @question.test
-    @client = client || Octokit::Client.new(:access_token => ACCESS_TOKEN)
+    @client = client || Octokit::Client.new(access_token: ENV['ACCESS_TOKEN'])
   end
 
   def call
